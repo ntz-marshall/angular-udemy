34a456bc03aed23d28855c226fab9691b9e8e75a
Marshall Nhemetz <swolv.lith@gmail.com>
2021-04-11 10:13:43 -0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgIfComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
